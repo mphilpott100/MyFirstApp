b3422d2d1e0c3ed620bdeb196036f8a6d7dddf3f
Markus Philpott <mphilpott100@gmail.com>
2020-06-17 14:42:53 -0500

    Change editText widget name to message

--- app/src/main/java/edu/xula/myfirstapp/MainActivity.java
+++ app/src/main/java/edu/xula/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.xula.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,10 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello Android Studio");
+        Log.i("E","F");
     }
 
     public void sendMessage (View view) {
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.message);
         String message = editText.getText().toString();
         Button button = (Button) findViewById(R.id.button);
         button.setText("Markus");
